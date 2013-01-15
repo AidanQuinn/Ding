5b0c6b334d910ab2d5fc42ce60d5f6d6aff64452
Tommaso Dalla Favera <tld2108@gmail.com>
2013-01-15 18:18:24 -0500

    fixed some bugs

--- contactlist_form.php
+++ contactlist_form.php
@@ -55,7 +55,8 @@ $headers = "From: info@dingconcussion.com\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-$message = '<html><body><img src="img/logo-min.png" style="width:150px; height=150px; float:left>"';
+$message = '<html><body><div style="width:100%;">';
+$message .= '<img src="img/logo-min.png" style="width:150px; height=150px; float:left">';
 $message .= '<h1>Hey ' . strip_tags($_POST['name']) . ',</h1> <br></br>';
 $message .= '<h2>Thanks for your interest in the DingMed iphone/ipad application. We&_#39ll let you know when we launch.</h2>';
 $message .= '</body></html>';
