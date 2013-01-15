b72552df35616fdf65f9d7fa9d3918241b20c10e
Tommaso Dalla Favera <tld2108@gmail.com>
2013-01-15 15:43:29 -0500

    Added email function to php
    body still needs html formatting

--- contactlist_form.php
+++ contactlist_form.php
@@ -4,10 +4,14 @@
 // This page receives the first name and emails from the coming soon page
 
 // Address error handling.
+
 ini_set ('display_errors', 1);
 error_reporting (E_ALL & ~E_NOTICE);
+
+//
 	
 // Connect and select.
+
 if (@mysql_connect('localhost', 'dingmed_aidan', 'BHprh7abJV'))
 {
 	if (!@mysql_select_db ('dingmed_contactList'))
@@ -19,20 +23,38 @@ else
 {
 	die (print mysql_error());
 }
+
+//
+
 // Define the query.
+
 $query = "INSERT INTO emailList
 (FirstName, email, dateEntered)
 VALUES ('{$_POST['name']}',
 '{$_POST['email']}', NOW())";
+
+//
+
 // Execute the query.
+
 if (@mysql_query ($query))
 {
-	print '<p> HMM ... WHAT THE F IS THIS GONNA LOOK LIKE?!? </p>';
+	;
 }
 else
 {
-	print '<p> DIDNT WORK ... WHAT DOES THIS LOOK LIKE?!? <b>' . mysql_error() . "/b>. The query was $query. </p>";
+	print mysql_error();
 }
 mysql_close();
+
+//
+
+// Send thanks/confirmation email.
+
+$body = ""
+
+mail ($_POST['email'], 'Thanks for signing up for the DingMed news letter!', $body, 'From: DINGEMAIL');
+
+//
 phpinfo();
 ?>
