b8352d02d8e77333289e978ad0bb4cc709f32da1
Tommaso Dalla Favera <tld2108@gmail.com>
2013-02-04 18:37:27 -0500

    fixed form reset

--- js/form_listener.js
+++ js/form_listener.js
@@ -7,15 +7,14 @@ var contactForm = {
 			event.preventDefault();
 			var name = $('form input#name').val();
 			var email = $('form input#email').val();
-			$('form#myform').remove();
-			//$('section.content article p').text('...');
 			try {
 				$.post('contactlist_form.php', {name: name, email: email}, function(data) {
-					//$('section.content article p').text('Thanks for your email!');	
+					//$('').text('Thanks for your email!');	
 				});
 			} catch (e) {
-				//$('section.content article p').text('Woops ... there seems to have been a problem accepting your email. Sorry. Please refresh the page and try again.');
+				//$('').text('Woops ... there seems to have been a problem accepting your email. Sorry. Please refresh the page and try again.');
 			}
+			$("input[placeholder]").val('');
 			setTimeout("$('div#myAlert').fadeIn(2000);", 500);
 			setTimeout("$('div#myAlert').fadeOut(2000);", 8000);
 		});
